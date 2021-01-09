b7b1cc645009c122df6f5e8f1a74502858c02457
Ioan-Andrei Ojica <ivan.andrei93@mail.ru>
2021-01-09 17:53:35 +0100

    added the sign up form

--- todoPROJ/urls.py
+++ todoPROJ/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todowoo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+
+    #Auth
+    path('admin/signup/', views.signupuser, name='signupuser')
+
+    #ToDos
 ]
--- todowoo/views.py
+++ todowoo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm 
 # Create your views here.
+
+def home(request):
+  return render(request, 'todowoo/home.html')
+
+def signupuser(request)
+  return render(request, 'todowoo/signupuser.html', {'form':UserCreationForm()})
+
